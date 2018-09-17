42d09f895c8643320cd6ad93e115c667f5305926
Vlad Zat <vlad.zat@mydit.ie>
2018-09-17 17:46:57 +0100

    More fetchedLatestVideos tests

--- src/components/lib/gapi-yt.js
+++ src/components/lib/gapi-yt.js
@@ -54,44 +54,49 @@ const gapiYT = {
             const playlistId = channelId[0] + 'U' + channelId.substring(2);
             let videos = [];
             let videoIds = '';
-            let nextPageToken = undefined;
 
-            // All videos have been fetched
-            if (earliestPublishedAt[channelId] === null) {
-                return videos;
+            if (videosFetched[channelId] === undefined) {
+                // 1. No videos have been fetched
+
+                videosLeft[channelId] = 0;
+                videosFetched[channelId] = 0;
+
+                // Get videos
+                const videoListPart = await window.gapi.client.youtube.playlistItems.list({
+                    playlistId: playlistId,
+                    part: 'snippet'
+                });
+
+                // Extract videos
+                for (let i = 0 ; i < videoListPart.result.items.length ; i ++) {
+                    videos.push(videoListPart.result.items[i].snippet);
+                    videoIds += videoListPart.result.items[i].snippet.resourceId.videoId;
+
+                    if (i + 1 < videoListPart.result.items.length) {
+                        videoIds += ',';
+                    }
+                    else {
+                        earliestPublishedAt[channelId] = new Date(videoListPart.result.items[i].snippet.publishedAt);
+                    }
+                }
             }
+            else if (videosFetched[channelId] + 5 <= 50) {
+                // 2. Videos have been fetched in the past, but the total amount is under 50
 
-            if (videosLeft[channelId] === undefined) videosLeft[channelId] = 0;
-            if (videosFetched[channelId] === undefined) videosFetched[channelId] = 0;
-            if (earliestPublishedAt[channelId] === undefined) earliestPublishedAt[channelId] = new Date();
-
-            while (videos.length < 5 && nextPageToken !== null) {
-                const maxResults = Math.min(videosFetched[channelId] + 5, 50);
-
-                // Get new videos
+                // Get Videos
                 const videoListPart = await window.gapi.client.youtube.playlistItems.list({
                     playlistId: playlistId,
                     part: 'snippet',
-                    nextPageToken,
-                    maxResults
+                    maxResults: videosFetched[channelId] + 5
                 });
 
-                // Get the nextPageToken
-                nextPageToken = videoListPart.result.nextPageToken;
-
-                // 1. No videos have been fetched
-                // 2. Videos have been fetched in the past but the total amount is under 50
-                // 3. Videos have been fetched in the past and the total amount is over 50
-
-                // Find the first video that has not been fetched before
+                // Find the first video with an earlier publishedAt date
                 let videoNo;
                 for (videoNo = 0 ; videoNo < videoListPart.result.items.length ; videoNo ++) {
                     const publishedAt = new Date(videoListPart.result.items[videoNo].snippet.publishedAt);
 
                     if (publishedAt < earliestPublishedAt[channelId]) break;
                 }
-
-                console.log(videoNo, videoListPart.result.items.length);
 
                 // Extract videos - start from the first video not fetched before
                 for (let i = videoNo ; i < videoListPart.result.items.length ; i ++) {
@@ -106,8 +111,51 @@ const gapiYT = {
                     }
                 }
 
-                // No more videos left to fetch
-                if (!nextPageToken) nextPageToken = null;
+                // No videos found, use the 3rd method
+                // This can happen when more than 5 videos are uploaded at the same time
+                if (videoNo >= videoListPart.result.items.length) {
+                    videosFetched[channelId] = 51;
+                }
+            }
+
+            if (videosFetched[channelId] + 5 > 50) {
+                // 3. Videos have been fetched in the past and the total amount is over 50
+
+                let nextPageToken;
+
+                while (videos.length === 0) {
+                    // Get Videos
+                    const videoListPart = await window.gapi.client.youtube.playlistItems.list({
+                        playlistId: playlistId,
+                        part: 'snippet',
+                        maxResults: 50,
+                        nextPageToken
+                    });
+
+                    // Get the nextPageToken
+                    nextPageToken = videoListPart.result.nextPageToken;
+
+                    // Find the first video with an earlier publishedAt date
+                    let videoNo;
+                    for (videoNo = 0 ; videoNo < videoListPart.result.items.length ; videoNo ++) {
+                        const publishedAt = new Date(videoListPart.result.items[videoNo].snippet.publishedAt);
+
+                        if (publishedAt < earliestPublishedAt[channelId]) break;
+                    }
+
+                    // Extract videos - start from the first video not fetched before
+                    for (let i = videoNo ; i < videoListPart.result.items.length ; i ++) {
+                        videos.push(videoListPart.result.items[i].snippet);
+                        videoIds += videoListPart.result.items[i].snippet.resourceId.videoId;
+
+                        if (i + 1 < videoListPart.result.items.length) {
+                            videoIds += ',';
+                        }
+                        else {
+                            earliestPublishedAt[channelId] = new Date(videoListPart.result.items[i].snippet.publishedAt);
+                        }
+                    }
+                }
             }
 
             // Get more data about videos if any videos have been found
@@ -117,8 +165,6 @@ const gapiYT = {
                     id: videoIds,
                     part: 'contentDetails,statistics'
                 });
-
-                // console.log(videos.length, videoListPartInfo.result.items.length);
 
                 for (let i = 0 ; i < videoListPartInfo.result.items.length ; i ++) {
                     videos[i] = {
@@ -133,8 +179,6 @@ const gapiYT = {
             videosLeft[channelId] += videos.length;
             videosFetched[channelId] += videos.length;
 
-            // console.log(videosLeft[channelId], videosFetched[channelId]);
-
             return videos;
         }
         catch (err) {
@@ -142,6 +186,100 @@ const gapiYT = {
             throw new Error(err);
         }
     },
+
+    // fetchLatestVideos: async (channelId) => {
+    //     try {
+    //         const playlistId = channelId[0] + 'U' + channelId.substring(2);
+    //         let videos = [];
+    //         let videoIds = '';
+    //         let nextPageToken = undefined;
+    //
+    //         // All videos have been fetched
+    //         if (earliestPublishedAt[channelId] === null) {
+    //             return videos;
+    //         }
+    //
+    //         if (videosLeft[channelId] === undefined) videosLeft[channelId] = 0;
+    //         if (videosFetched[channelId] === undefined) videosFetched[channelId] = 0;
+    //         if (earliestPublishedAt[channelId] === undefined) earliestPublishedAt[channelId] = new Date();
+    //
+    //         while (videos.length < 5 && nextPageToken !== null) {
+    //             const maxResults = Math.min(videosFetched[channelId] + 5, 50);
+    //
+    //             // Get new videos
+    //             const videoListPart = await window.gapi.client.youtube.playlistItems.list({
+    //                 playlistId: playlistId,
+    //                 part: 'snippet',
+    //                 nextPageToken,
+    //                 maxResults
+    //             });
+    //
+    //             // Get the nextPageToken
+    //             nextPageToken = videoListPart.result.nextPageToken;
+    //
+    //             // 1. No videos have been fetched
+    //             // 2. Videos have been fetched in the past but the total amount is under 50
+    //             // 3. Videos have been fetched in the past and the total amount is over 50
+    //
+    //             // Find the first video that has not been fetched before
+    //             let videoNo;
+    //             for (videoNo = 0 ; videoNo < videoListPart.result.items.length ; videoNo ++) {
+    //                 const publishedAt = new Date(videoListPart.result.items[videoNo].snippet.publishedAt);
+    //
+    //                 if (publishedAt < earliestPublishedAt[channelId]) break;
+    //             }
+    //
+    //             console.log(videoNo, videoListPart.result.items.length);
+    //
+    //             // Extract videos - start from the first video not fetched before
+    //             for (let i = videoNo ; i < videoListPart.result.items.length ; i ++) {
+    //                 videos.push(videoListPart.result.items[i].snippet);
+    //                 videoIds += videoListPart.result.items[i].snippet.resourceId.videoId;
+    //
+    //                 if (i + 1 < videoListPart.result.items.length) {
+    //                     videoIds += ',';
+    //                 }
+    //                 else {
+    //                     earliestPublishedAt[channelId] = new Date(videoListPart.result.items[i].snippet.publishedAt);
+    //                 }
+    //             }
+    //
+    //             // No more videos left to fetch
+    //             if (!nextPageToken) nextPageToken = null;
+    //         }
+    //
+    //         // Get more data about videos if any videos have been found
+    //         if (videoIds.length > 0) {
+    //             // Get more data on the videos
+    //             const videoListPartInfo = await window.gapi.client.youtube.videos.list({
+    //                 id: videoIds,
+    //                 part: 'contentDetails,statistics'
+    //             });
+    //
+    //             // console.log(videos.length, videoListPartInfo.result.items.length);
+    //
+    //             for (let i = 0 ; i < videoListPartInfo.result.items.length ; i ++) {
+    //                 videos[i] = {
+    //                   ...videos[i],
+    //                   ...videoListPartInfo.result.items[i].contentDetails,
+    //                   ...videoListPartInfo.result.items[i].statistics
+    //                 };
+    //             }
+    //         }
+    //
+    //         // Store the number of videos left to process and the total amount of videos fetched
+    //         videosLeft[channelId] += videos.length;
+    //         videosFetched[channelId] += videos.length;
+    //
+    //         // console.log(videosLeft[channelId], videosFetched[channelId]);
+    //
+    //         return videos;
+    //     }
+    //     catch (err) {
+    //         console.log(err);
+    //         throw new Error(err);
+    //     }
+    // },
     initStagingArea: async () => {
         try {
             let promises = [];
