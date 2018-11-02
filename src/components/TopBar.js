7aa205d94ef6f8ef7a6b4c257c962ec42f56ee44
Vlad Zat <vlad.zat@mydit.ie>
2018-11-02 13:46:07 +0000

    Add new list

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './css/TopBar.css';
 
-import { DropdownButton, MenuItem, Glyphicon } from 'react-bootstrap';
+import { DropdownButton, MenuItem, Glyphicon, Modal, Button, Alert } from 'react-bootstrap';
 
 class TopBar extends Component {
     state = {
         videoLists: {},
-        currentList: 'All'
+        currentList: 'All',
+        newListModalOpened: false,
+        newVideoListName: '',
+        showVideoListNameAlert: false
     }
 
     componentDidMount = () => {
@@ -35,11 +38,61 @@ class TopBar extends Component {
         await this.setState({videoLists});
     }
 
+    showNewListModal = () => {
+        this.setState({newListModalOpened: true});
+    }
+
+    hideNewListModal = () => {
+        this.setState({newListModalOpened: false});
+
+        // Close warning
+        if (this.state.showVideoListNameAlert) {
+            this.setState({showVideoListNameAlert: false});
+        }
+
+        // Clean Input
+        this.setState({newVideoListName: ''});
+    }
+
+    onListSelect = (eventKey) => {
+        // Add new list
+        if (eventKey === 'addNewList') {
+            this.showNewListModal();
+        }
+        else if (this.state.videoLists[eventKey]) {
+            this.setState({currentList: eventKey});
+        }
+    }
+
+    handleListNameChange = (event, comp) => {
+        this.setState({newVideoListName: event.target.value});
+    }
+
+    handleNewList = () => {
+        const { videoLists } = this.state;
+
+        if (!videoLists[this.state.newVideoListName]) {
+            videoLists[this.state.newVideoListName] = [];
+
+            // Create the new list
+            this.setState({videoLists});
+            localStorage.setItem('video-lists', JSON.stringify(videoLists));
+
+            // CloseModal
+            this.hideNewListModal();
+
+        }
+        else {
+            // Display error
+            this.setState({showVideoListNameAlert: true});
+        }
+    }
+
     render() {
         const { videoLists } = this.state;
 
         const lists = Object.keys(videoLists).map((list, idx) => (
-            <MenuItem className = 'video-list' eventKey = {idx}> {list} </MenuItem>
+            <MenuItem className = 'video-list' eventKey = {list}> {list} </MenuItem>
         ));
 
         return (
@@ -47,13 +100,43 @@ class TopBar extends Component {
                 <div className = 'logo'> Video List </div>
 
                 <div className = 'content'>
-                    <DropdownButton className = 'video-lists' title = {this.state.currentList}>
+                    <DropdownButton className = 'video-lists' title = {this.state.currentList} onSelect = {this.onListSelect} >
                         {lists}
-                        <MenuItem className = 'video-list' eventKey = 'new'>
+                        <MenuItem divider />
+                        <MenuItem className = 'video-list' eventKey = 'addNewList'>
                             <Glyphicon glyph='plus'/> New List
                         </MenuItem>
                     </DropdownButton>
                 </div>
+
+                <Modal show = {this.state.newListModalOpened} onHide = {this.hideNewListModal}>
+                    <Modal.Header>
+                        <Modal.Title> Add a new Video List </Modal.Title>
+                    </Modal.Header>
+
+                    <Modal.Body>
+                        <input
+                            id = 'add-video-list'
+                            name = 'add-video-list'
+                            className = 'video-list-input'
+                            placeholder = 'List name'
+                            value = {this.state.newVideoListName}
+                            onChange = {this.handleListNameChange} />
+
+                        <Button
+                            className = 'video-list-submit'
+                            onClick = {this.handleNewList} >
+                                Submit
+                        </Button>
+
+                        { this.state.showVideoListNameAlert &&
+                            <Alert bsStyle = 'warning'>
+                                A list with this name already exists.
+                            </Alert>
+                        }
+
+                    </Modal.Body>
+                </Modal>
             </div>
         );
     }
